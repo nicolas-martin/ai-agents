9d4cd95201ca10ba054a53f0830de783b99bc5d8
Nicolas Martin <nicolas.martin.0986@gmail.com>
2026-01-05 12:15:58 -0500

    --wip--

--- src/strategies/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-🌙 Moon Dev's Strategies Package
-"""
-
-from .base_strategy import BaseStrategy
-
-# We only need to export BaseStrategy - custom strategies will be loaded dynamically
-__all__ = ['BaseStrategy'] 
